b22bcb734c8d6c1b2e5e8c2c2d63d29ef50f241c
Gianpaolo Macario <gmacario@gmail.com>
2014-08-30 10:30:33 +0200

    Prepare restore of CONFLUENCE_BACKUP
    Signed-off-by: Gianpaolo Macario <gmacario@gmail.com>

--- install-confluence.sh
+++ install-confluence.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 # ==================================================================================================
 # Script to download and install Atlassian Confluence
 #
@@ -7,7 +6,7 @@
 #	https://maxlab.polito.it/confluence/x/DAAb
 # ==================================================================================================
 
-set -x
+#set -x
 
 DOWNLOAD_DIR=`pwd`/tmp
 #DOWNLOAD_DIR=$/Downloads
@@ -21,14 +20,15 @@ cd ${DOWNLOAD_DIR}
 # Install dependencies for Atlassian Confluence
 # (none identified so far)
 
-# Download Atlassian Confluence
 [ ! -z "${CONFLUENCE_INSTALLFILE}" ] && if [ ! -e ${CONFLUENCE_INSTALLFILE} ]; then
+    echo "INFO: Downloading ${CONFLUENCE_INSTALLFILE}"
     wget http://www.atlassian.com/software/confluence/downloads/binary/${CONFLUENCE_INSTALLFILE}
     chmod 755 ${CONFLUENCE_INSTALLFILE}
 fi
 
 # Download maxlab.polito.it/confluence backup
 [ ! -z "${CONFLUENCE_BACKUP}" ] && if [ ! -e ${CONFLUENCE_BACKUP} ]; then
+    echo "INFO: Downloading ${CONFLUENCE_BACKUP}"
     scp gmacario@maxlab.polito.it:/var/atlassian/application-data/confluence/backups/${CONFLUENCE_BACKUP} .
 fi
 
@@ -37,7 +37,8 @@ if [ `whoami` != root ]; then
 else
     SUDO=""
 fi
-# Install Atlassian Confluence
+
+echo "INFO: Installing ${CONFLUENCE_INSTALLFILE}"
 ${SUDO} ${DOWNLOAD_DIR}/${CONFLUENCE_INSTALLFILE} <<__END__
 o
 2
@@ -46,5 +47,12 @@ o
 1
 y
 __END__
+
+if [ ! -z "${CONFLUENCE_BACKUP}" ]; then
+    echo "INFO: Preparing restore of ${CONFLUENCE_BACKUP}..."
+    ${SUDO} mkdir -p /var/atlassian/application-data/confluence/restore
+    ${SUDO} chown confluence /var/atlassian/application-data/confluence/restore
+    ${SUDO} cp ${DOWNLOAD_DIR}/${CONFLUENCE_BACKUP} /var/atlassian/application-data/confluence/restore/
+fi
 
 # END
